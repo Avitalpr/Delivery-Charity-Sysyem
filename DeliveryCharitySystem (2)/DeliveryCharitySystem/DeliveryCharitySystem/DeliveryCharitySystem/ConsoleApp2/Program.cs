a7228f96f817e278828b79e3a21cb593f2c79c89
AVITAL <avitalish446@gmail.com>
2020-09-30 09:40:42 +0300

    COMMIT FINAL

---
+++ DeliveryCharitySystem (2)/DeliveryCharitySystem/DeliveryCharitySystem/DeliveryCharitySystem/ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using DC_SYSTEM.DAL;
+using DC_SYSTEM._BE;
+
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			using (var ctx = new DeliveryContext())
+			{
+				var adult = new Adult() { Id = "123456" };
+
+				ctx.Adults.Add(adult);
+				ctx.SaveChanges();
+			}
+
+
+			//			Accord.Math.Random.Generator.Seed = 0;
+
+			//            // A common desire when doing clustering is to attempt to find how to 
+			//            // weight the different components / columns of a dataset, giving them 
+			//            // different importances depending on the end goal of the clustering task.
+
+			//            // Declare some observations
+			//            double[][] observations =
+			//            {
+			//    new double[] { -5, -2, -1 },
+			//    new double[] { -5, -5, -6 },
+			//    new double[] {  2,  1,  1 },
+			//    new double[] {  1,  1,  2 },
+			//    new double[] {  1,  2,  2 },
+			//    new double[] {  3,  1,  2 },
+			//    new double[] { 11,  5,  4 },
+			//    new double[] { 15,  5,  6 },
+			//    new double[] { 10,  5,  6 },
+			//};
+
+			//            // Create a new K-Means algorithm
+			//            KMeans kmeans = new KMeans(k: 3)
+			//            {
+			//                // For example, let's say we would like to consider the importance of 
+			//                // the first column as 0.1, the second column as 0.7 and the third 0.9
+			//                Distance = new WeightedSquareEuclidean(new double[] { 0.1, 0.7, 1.1 })
+			//            };
+
+			//            // Compute and retrieve the data centroids
+			//            var clusters = kmeans.Learn(observations);
+
+			//            // Use the centroids to parition all the data
+			//            int[] labels = clusters.Decide(observations);
+			//           // for(int i=0;i<labels.Length;i++)
+			//                foreach(var a in clusters)
+			//				{
+			//                  //  Console.WriteLine(a.);
+			//                }
+
+
+		}
+	}
+}
+
+
+
+
+
---
+++ DeliveryCharitySystem (2)/DeliveryCharitySystem/DeliveryCharitySystem/DeliveryCharitySystem/DC_SYSTEM/Windows/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DC_SYSTEM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DC_SYSTEM.Windows
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		/// <summary>
+		/// Interaction logic for addingOld_usctrl.xaml
+		/// </summary>
+
+			public AdultViewModel adultMV;
+
+			public Window1()
+			{
+
+				//adultMV = new AdultViewModel();
+				//this.DataContext = this.adultMV;
+				InitializeComponent();
+
+			}
+
+			//public AddAdult_userCntrl(AdultViewModel adultMV)
+			//{
+			//	this.adultMV = adultMV;
+			//}
+		}
+	}
